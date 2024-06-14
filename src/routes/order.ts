ba7900380a6c582205a7c0d2e137af4e11a9b9f5
Nicolas FOUQUE <nicolas.fouque@laposte.net>
2024-06-14 16:35:52 +0200

    add Livreur back

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -3,21 +3,80 @@ import { MongoClient } from 'mongodb';
 
 const orderRouter = Router();
 
+
 orderRouter.get('/getOrders', async (req: any, res: any) => {
+    const uri = "mongodb+srv://admin:adminces'eat@ceseat.rkfov9n.mongodb.net/CES'EAT";
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        // console.log("Connected to MongoDB");
+
+        const database = client.db();
+        const commandesCollection = database.collection("Commandes");
+
+        const orders = await commandesCollection.find().toArray(); res.json(orders);
+
+    } catch (e) {
+        console.error('Error fetching commandes:', e);
+        res.status(500).json({ message: 'Internal Server Error' });
+    } finally {
+        await client.close();
+    }
+})
+
+orderRouter.post('/assignDeliveryman', async (req: any, res: any) => {
     const uri = "mongodb+srv://admin:adminces'eat@ceseat.rkfov9n.mongodb.net/CES'EAT";
     const client = new MongoClient(uri);
 
     try {
         await client.connect();
-        console.log("Connected to MongoDB");
+        // console.log("Connected to MongoDB");
 
         const database = client.db();
         const commandesCollection = database.collection("Commandes");
 
-        const orders = await commandesCollection.find().toArray();
-        console.log(orders);
-        res.json(orders);
+        const { idOrder, idDriver, nameDriver, phoneDriver } = req.body;
 
+        await commandesCollection.updateOne(
+            { order_id: idOrder },
+            {
+                $set: {
+                    "driver.driver_id": idDriver,
+                    "driver.name": nameDriver,
+                    "driver.phone": phoneDriver,
+                }
+            }
+        )
+    } catch (e) {
+        console.error('Error fetching commandes:', e);
+        res.status(500).json({ message: 'Internal Server Error' });
+    } finally {
+        await client.close();
+    }
+})
+
+orderRouter.post('/updateOrderStatus', async (req: any, res: any) => {
+    const uri = "mongodb+srv://admin:adminces'eat@ceseat.rkfov9n.mongodb.net/CES'EAT";
+    const client = new MongoClient(uri);
+    
+    try {
+        await client.connect();
+        // console.log("Connected to MongoDB");
+
+        const database = client.db();
+        const commandesCollection = database.collection("Commandes");
+
+        const { idOrder, newStatus } = req.body;
+        console.log(idOrder, newStatus)
+
+        await commandesCollection.updateOne(
+            { order_id: idOrder },
+            {
+                $set: {
+                    "order_status": newStatus,
+                }
+            }
+        )
     } catch (e) {
         console.error('Error fetching commandes:', e);
         res.status(500).json({ message: 'Internal Server Error' });
