3cfc53a32d1a86bb3216993ff681cd15fc6ce567
Nicolas FOUQUE <nicolas.fouque@laposte.net>
2024-06-19 14:15:55 +0200

    add create + update (Menu & Article)

--- src/routes/article.ts
+++ src/routes/article.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { AppDataSource } from '../config';
 import { Article } from '../entities/article';
+import { MongoClient } from 'mongodb';
 
 const articleRouter = Router();
 
@@ -14,5 +15,40 @@ articleRouter.get('/getArticles', async (req: any, res: any) => {
 
     res.status(200).json(articles);
 });
+
+articleRouter.post('/createArticle', async (req: any, res: any) => {
+
+    const { name_article, category_article, price_article, id_restorer } = req.body;
+
+    const articleRepository = AppDataSource.getRepository(Article);
+   
+        const article = new Article();
+        article.name_article = name_article;
+        article.category_article = category_article;
+        article.price_article = price_article;
+        article.id_restorer = id_restorer
+
+
+        await articleRepository.save(article);
+
+        return res.status(200).json({ message: "Création de l'article réussie" });
+});
+
+articleRouter.post('/updateArticle', async (req: any, res: any) => {
+
+    const { id_article, name_article, category_article, price_article, id_restorer } = req.body;
+
+    const articleRepository = AppDataSource.getRepository(Article);
+    const article = await articleRepository.findOne({ where: { id_article: id_article } });
+    article!.name_article = name_article;
+    article!.category_article = category_article;
+    article!.price_article = price_article;
+    article!.id_restorer = id_restorer
+
+        await articleRepository.save(article!);
+
+        return res.status(200).json({ message: "Mise à jour de l'article réussie" });
+});
+
 
 export default articleRouter;
--- src/routes/order.ts
+++ src/routes/order.ts
@@ -6,19 +6,42 @@ const orderRouter = Router();
 orderRouter.get('/getOrders', async (req: any, res: any) => {
     const uri = "mongodb+srv://admin:adminces'eat@ceseat.rkfov9n.mongodb.net/CES'EAT";
     const client = new MongoClient(uri);
-
     try {
         await client.connect();
-        console.log("Connected to MongoDB");
 
         const database = client.db();
         const commandesCollection = database.collection("Commandes");
 
         const orders = await commandesCollection.find().toArray();
         res.json(orders);
-
     } catch (e) {
         console.error('Error fetching commandes:', e);
+        res.status(500).json({ message: 'Internal Server Error' });
+    } finally {
+        await client.close();
+    }
+})
+
+orderRouter.post('/updateOrderStatus', async (req: any, res: any) => {
+    const uri = "mongodb+srv://admin:adminces'eat@ceseat.rkfov9n.mongodb.net/CES'EAT";
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        const database = client.db();
+        const commandesCollection = database.collection("Commandes");
+        const { idOrder, newStatus } = req.body;
+
+        await commandesCollection.updateOne(
+            { order_id: idOrder },
+            {
+                $set: {
+                    "order_status": newStatus,
+                }
+            }
+        )
+        res.status(200).json({ message: 'Mise à jour du status de la commande réussite' });
+    } catch (e) {
+        console.error('Error Update Error Status:', e);
         res.status(500).json({ message: 'Internal Server Error' });
     } finally {
         await client.close();
